c0859e1507adb72da1fc7fb3c1059156ff8e7720
Devin Alder <brettdevinalder@gmail.com>
2026-09-21 09:59:11 -0600

    ad

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Formats.Tar;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        float gradePercentage;
+        string gradeString;
+        string sign;
+
+        Console.Write("Enter your grade percentage ");
+        gradeString = Console.ReadLine();
+        gradePercentage = int.Parse(gradeString);
+        if (gradePercentage % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (gradePercentage % 10 >= 7)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine($"You have an A");
+        }
+        else if (gradePercentage < 90 && gradePercentage >= 80)
+        {
+            Console.WriteLine("You have an B");
+        }
+        else if (gradePercentage < 80 && gradePercentage >= 70)
+        {
+            Console.WriteLine("You have an C");
+        }
+        else if (gradePercentage < 70 && gradePercentage >= 60)
+        {
+            Console.WriteLine("You have an D");
+        }
+        else
+        {
+            Console.WriteLine("You have an F");
+        }
+        if (gradePercentage > 70)
+        {
+            Console.WriteLine("You passed!");
+        }
     }
 }
